52c2468564d7bcd78b2779e8a69a791d13d7f89f
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2024-04-02 14:28:30 +0200

    ci: better move script

--- ByeByeLandmine/move.bat
+++ ByeByeLandmine/move.bat
@@ -4,10 +4,7 @@ FOR %%I IN (.) DO SET CurrentD=%%~nI%%~xI
 
 md "%APPDATA%\Thunderstore Mod Manager\DataFolder\LethalCompany\profiles\Default\BepInEx\plugins\%CurrentD%\" 2> nul
 
-xcopy /Y ".\bin\Debug\netstandard2.1\*.dll" "%APPDATA%\Thunderstore Mod Manager\DataFolder\LethalCompany\profiles\Default\BepInEx\plugins\%CurrentD%\"
-
 xcopy /Y ".\bin\Debug\netstandard2.1\*.dll" "..\thunderstore\"
-
-xcopy /Y ".\AssetBundles" "%APPDATA%\Thunderstore Mod Manager\DataFolder\LethalCompany\profiles\Default\BepInEx\plugins\%CurrentD%\assets\"
-
 xcopy /Y ".\AssetBundles" "..\thunderstore\assets\"
+
+xcopy /Y "..\thunderstore" "%APPDATA%\Thunderstore Mod Manager\DataFolder\LethalCompany\profiles\Default\BepInEx\plugins\%CurrentD%\"
