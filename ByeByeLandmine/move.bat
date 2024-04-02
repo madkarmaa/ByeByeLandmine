301c697bd7fead99f9af4bd6ce7dd661b5b36bc3
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2024-04-02 18:07:40 +0200

    fix: missing files and wrong directory name

--- ByeByeLandmine/move.bat
+++ ByeByeLandmine/move.bat
@@ -4,7 +4,7 @@ FOR %%I IN (.) DO SET CurrentD=%%~nI%%~xI
 
 md "%APPDATA%\Thunderstore Mod Manager\DataFolder\LethalCompany\profiles\Default\BepInEx\plugins\%CurrentD%\" 2> nul
 
-xcopy /Y ".\bin\Debug\netstandard2.1\*.dll" "..\thunderstore\"
-xcopy /Y ".\AssetBundles" "..\thunderstore\assets\"
+xcopy /Y /S ".\bin\Debug\netstandard2.1\*.dll" "..\thunderstore\"
+xcopy /Y /S ".\AssetBundles" "..\thunderstore\assets\"
 
-xcopy /Y "..\thunderstore" "%APPDATA%\Thunderstore Mod Manager\DataFolder\LethalCompany\profiles\Default\BepInEx\plugins\%CurrentD%\"
+xcopy /Y /S "..\thunderstore" "%APPDATA%\Thunderstore Mod Manager\DataFolder\LethalCompany\profiles\Default\BepInEx\plugins\%CurrentD%\"
